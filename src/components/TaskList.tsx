9ef25f715155322b9fe65ab84fbcea099f774722
Ednaldo Ferreira <naldo_a.f@hotmail.com>
2021-09-22 08:16:56 -0300

    Desafio Finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,26 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    setTasks([...tasks,{
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }]);
+
+    setNewTaskTitle('');
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(eachTask => eachTask.id === id ? {...eachTask, isComplete: !eachTask.isComplete} : eachTask ))    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(eachTask => eachTask.id !== id));
   }
 
   return (
